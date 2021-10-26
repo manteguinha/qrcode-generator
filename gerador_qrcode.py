94469c6d95f7bfab1f7d53d027476dd79c78cb81
Marcos Vinicius <63943591+manteguinha@users.noreply.github.com>
2021-10-26 10:53:29 -0300

    0.2.0

--- gerador_qrcode.py
+++ gerador_qrcode.py
@@ -1,16 +1,17 @@
 import qrcode
+from tkinter.font import BOLD
 import PySimpleGUI as sg
 
 class Tela:
     sg.theme('Dark')
     def __init__(self) :
         layout = [
-            [sg.Text('Link', size=(4,0),  font=("Arial", 14)), sg.Input(size=(55,0),  font=("Arial", 12) , key='_LINK_')],  
-            [sg.Text('Onde deseja salvar a imagem?',  font=("Arial", 12))],
-            [ sg.FolderBrowse('Buscar',  font=("Arial", 12) , key='_BUSCAR_', button_color=('White', 'Gray')),sg.Input(size=(51,0), font=("Arial", 12))],   
-            [sg.Button('Criar QR Code',  font=("Arial", 12) , button_color=('White', 'Green'))]
+            [sg.Text('Link', size=(5,0)), sg.Input(size=(40,0), key='_LINK_')],  
+            [sg.Text('Onde deseja salvar a imagem?')],
+            [sg.FolderBrowse('Buscar', font=("Arial", 10, BOLD) , key='_BUSCAR_', button_color=('White', 'Gray')),sg.Input(size=(40,0))],   
+            [sg.Button('Criar QR Code', font=("Arial", 10, BOLD) , button_color=('White', 'Green'))]
         ]
-        self.janela = sg.Window("Gerador de codigo QR").layout(layout)
+        self.janela = sg.Window("Gerador de codigo QR", font=("Arial", 12)).layout(layout)
 
     def Iniciar(self):
         while True:
@@ -20,13 +21,13 @@ class Tela:
             
             if self.button == 'Criar QR Code':
                 if self.values['_LINK_'] == '' or self.values['_BUSCAR_'] == '':
-                    sg.Popup('Há campos vazios a serem preenchidos',  font=("Arial", 14) , title='Atenção')
+                    sg.Popup('Há campos vazios a serem preenchidos', title='Atenção')
                 else:
                     link = self.values['_LINK_']
                     imagem_qr = qrcode.make(link)
                     caminho = self.values['_BUSCAR_']
                     imagem_qr.save(caminho+'\imagem_qrcode.png')
-                    sg.Popup(f'O codigo QR foi salvo em: {caminho}',  font=("Arial", 14) , title='Sucesso!')
+                    sg.Popup(f'O codigo QR foi salvo em: {caminho}', title='Sucesso!')
                 
 tela = Tela()
 tela.Iniciar()
